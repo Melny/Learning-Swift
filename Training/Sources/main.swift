c25e125d83edf14db6cd90f2935cdd1e6692ef92
Melny <marek.melnycok@gmail.com>
2025-03-28 19:27:16 +0100

    Add main code

--- Training/Sources/main.swift
+++ Training/Sources/main.swift
@@ -1,4 +1,25 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-print("Hello, world!")
+import Foundation
+
+func past(_ h: Int, _ m: Int, _ s: Int) -> Int {
+    let hoursAndMinutesToSecond = (((h * 60) + m) * 60) + s
+    let miliseconde = hoursAndMinutesToSecond * 1000
+    return miliseconde
+}
+
+print(past(1, 20, 34))
+
+func reverse(_ str: String) -> String {
+    var myCharacter = Array(str)
+    myCharacter.reverse()
+    let backToString = String(myCharacter)
+    return backToString
+}
+
+print(reverse("Marek"))
+
+func warnTheSheep(_ queue: [String]) -> String {
+    return ""
+}
